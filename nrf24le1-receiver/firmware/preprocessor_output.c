3974e785674f9eb4195e672451594bdafcc6c159
Werner Lane <wl-opensource@gmx.net>
2014-12-26 17:39:27 +0800

    Fix preprocessor; not tested yet

--- nrf24le1-receiver/firmware/preprocessor_output.c
+++ nrf24le1-receiver/firmware/preprocessor_output.c
@@ -23,6 +23,8 @@
     #define TX_DATA_SIZE 4
 #endif
 
+#define NUMBER_OF_STARTUP_PACKETS 20
+
 extern bool systick;
 extern __xdata uint16_t channels[NUMBER_OF_CHANNELS];
 extern __xdata uint16_t raw_data[2];
@@ -91,23 +93,25 @@ static void normalize_channel(CHANNEL_T *c)
 // ****************************************************************************
 void output_preprocessor(void)
 {
+    static uint8_t startup_count = 0;
+
     if (systick) {
-        if (successful_stick_data) {
-            if (!initialized) {
-                initialized = true;
-                servo[0].centre = channels[0];
-                servo[1].centre = channels[1];
-
-                servo[0].left = channels[0] - INITIAL_ENDPOINT_DELTA;
-                servo[0].right = channels[0] + INITIAL_ENDPOINT_DELTA;
-                servo[1].left = channels[1] - INITIAL_ENDPOINT_DELTA;
-                servo[1].right = channels[1] + INITIAL_ENDPOINT_DELTA;
-            }
-
+        if (successful_stick_data && startup_count >= NUMBER_OF_STARTUP_PACKETS) {
             // Multiply by 0.75 to get microseconds from 750ns based clock
             servo[0].raw_data = channels[0] * 3 / 4;
             servo[1].raw_data = channels[1] * 3 / 4;
             ch3_raw = channels[2] * 3 / 4;
+
+            if (!initialized) {
+                initialized = true;
+                servo[0].centre = servo[0].raw_data;
+                servo[1].centre = servo[1].raw_data;
+
+                servo[0].left = servo[0].centre - INITIAL_ENDPOINT_DELTA;
+                servo[0].right = servo[0].centre + INITIAL_ENDPOINT_DELTA;
+                servo[1].left = servo[1].centre - INITIAL_ENDPOINT_DELTA;
+                servo[1].right = servo[1].centre + INITIAL_ENDPOINT_DELTA;
+            }
 
             normalize_channel(&servo[0]);
             normalize_channel(&servo[1]);
@@ -128,6 +132,9 @@ void output_preprocessor(void)
             tx_data[3] = ch3_2pos ? 1 : 0;
         }
         else {
+            if (startup_count < NUMBER_OF_STARTUP_PACKETS) {
+                ++startup_count;
+            }
             tx_data[1] = 0;
             tx_data[2] = 0;
             tx_data[3] = 0 + (1 << 4);          // CH3 + STARTUP_MODE flag
