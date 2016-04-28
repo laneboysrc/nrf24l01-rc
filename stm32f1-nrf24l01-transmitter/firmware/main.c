712a252fb8448fb5736c48ca9ac489614fd3ab19
Werner Lane <laneboysrc@gmail.com>
2016-04-28 14:31:53 +0800

    Optimize periperhal clock enabling

--- stm32f1-nrf24l01-transmitter/firmware/main.c
+++ stm32f1-nrf24l01-transmitter/firmware/main.c
@@ -46,11 +46,11 @@ static void init_systick(void)
 // ****************************************************************************
 static void init_gpio(void)
 {
-    rcc_periph_clock_enable(RCC_GPIOC);
-    gpio_set_mode(GPIOC, GPIO_MODE_OUTPUT_2_MHZ, GPIO_CNF_OUTPUT_PUSHPULL, GPIO13);
+    // Enable clocks for GPIO port A (for GPIO_USART1_TX) and C (LED)
+    rcc_periph_clock_enable(RCC_GPIOC | RCC_GPIOA);
 
-    // Enable clocks for GPIO port A (for GPIO_USART1_TX)
-    rcc_periph_clock_enable(RCC_GPIOA);
+    // Configure LED output port
+    gpio_set_mode(GPIOC, GPIO_MODE_OUTPUT_2_MHZ, GPIO_CNF_OUTPUT_PUSHPULL, GPIO13);
 }
 
 
