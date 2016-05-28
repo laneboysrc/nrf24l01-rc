63a3c0beff2156ad1e20a8044721a0d626a1d729
LANE Boys RC <laneboysrc@gmail.com>
2016-05-28 09:25:15 +0800

    Handle NSS ourself and don't turn off the SPI after transfers

--- stm32f1-nrf24l01-transmitter/firmware/system/spi.c
+++ stm32f1-nrf24l01-transmitter/firmware/system/spi.c
@@ -15,19 +15,26 @@ void init_spi(void)
 
     // Configure GPIOs: SS=PB12, SCK=PB13, MISO=PB14 and MOSI=PB15
     gpio_set_mode(GPIOB, GPIO_MODE_OUTPUT_50_MHZ,
-            GPIO_CNF_OUTPUT_ALTFN_PUSHPULL, GPIO12 | GPIO13 | GPIO15);
+            GPIO_CNF_OUTPUT_ALTFN_PUSHPULL, GPIO13 | GPIO15);
+
+    gpio_set_mode(GPIOB, GPIO_MODE_OUTPUT_50_MHZ,
+            GPIO_CNF_OUTPUT_PUSHPULL, GPIO12);
+
     gpio_set_mode(GPIOA, GPIO_MODE_INPUT, GPIO_CNF_INPUT_FLOAT, GPIO14);
 
     // Reset SPI, SPI_CR1 register cleared, SPI is disabled
     spi_reset(SPI2);
 
     // Set up SPI2 in Master mode
-    spi_init_master(SPI2, SPI_CR1_BAUDRATE_FPCLK_DIV_64,
+    spi_init_master(SPI2, SPI_CR1_BAUDRATE_FPCLK_DIV_16,
         SPI_CR1_CPOL_CLK_TO_0_WHEN_IDLE, SPI_CR1_CPHA_CLK_TRANSITION_1,
         SPI_CR1_DFF_8BIT, SPI_CR1_MSBFIRST);
 
-    // Enable hardware NSS
-    spi_enable_ss_output(SPI2);
+    spi_enable_software_slave_management(SPI2);
+    spi_disable_ss_output(SPI2);
+    spi_set_nss_high(SPI2);
+
+    spi_enable(SPI2);
 }
 
 
@@ -36,13 +43,21 @@ uint8_t spi_transaction(unsigned int count, uint8_t *buffer)
 {
     spi_enable(SPI2);
 
+    gpio_clear(GPIOB, GPIO12);
+
+
     for (unsigned int i = 0; i < count; i++) {
          uint8_t rx = (uint8_t)spi_xfer(SPI2, buffer[i]);
          buffer[i] = rx;
     }
 
+    /* Wait until not busy */
+    // while (SPI_SR(SPI2) & SPI_SR_BSY);
+
     // Disable SPI2 to release NSS
-    spi_disable(SPI2);
+    // spi_disable(SPI2);
+
+    gpio_set(GPIOB, GPIO12);
 
     return *buffer;
 }
