cd01e589f98338b6b72f672fe75687c8666aa4cb
LANE Boys RC <laneboysrc@gmail.com>
2016-05-31 11:18:47 +0800

    Tidy up SPI code

--- stm32f1-nrf24l01-transmitter/firmware/system/spi.c
+++ stm32f1-nrf24l01-transmitter/firmware/system/spi.c
@@ -14,12 +14,8 @@ void SPI_init(void)
     rcc_periph_clock_enable(RCC_SPI2);
 
     // Configure GPIOs: SS=PB12, SCK=PB13, MISO=PB14 and MOSI=PB15
-    gpio_set_mode(GPIOB, GPIO_MODE_OUTPUT_50_MHZ,
-            GPIO_CNF_OUTPUT_ALTFN_PUSHPULL, GPIO13 | GPIO15);
-
-    gpio_set_mode(GPIOB, GPIO_MODE_OUTPUT_50_MHZ,
-            GPIO_CNF_OUTPUT_PUSHPULL, GPIO12);
-
+    gpio_set_mode(GPIOB, GPIO_MODE_OUTPUT_50_MHZ, GPIO_CNF_OUTPUT_ALTFN_PUSHPULL, GPIO13 | GPIO15);
+    gpio_set_mode(GPIOB, GPIO_MODE_OUTPUT_50_MHZ, GPIO_CNF_OUTPUT_PUSHPULL, GPIO12);
     gpio_set_mode(GPIOA, GPIO_MODE_INPUT, GPIO_CNF_INPUT_FLOAT, GPIO14);
 
     // Reset SPI, SPI_CR1 register cleared, SPI is disabled
@@ -30,6 +26,7 @@ void SPI_init(void)
         SPI_CR1_CPOL_CLK_TO_0_WHEN_IDLE, SPI_CR1_CPHA_CLK_TRANSITION_1,
         SPI_CR1_DFF_8BIT, SPI_CR1_MSBFIRST);
 
+    // Configure chip select (SS) to be controlled by software
     spi_enable_software_slave_management(SPI2);
     spi_disable_ss_output(SPI2);
     spi_set_nss_high(SPI2);
@@ -42,22 +39,13 @@ void SPI_init(void)
 uint8_t SPI_transaction(unsigned int count, uint8_t *buffer)
 {
     gpio_clear(GPIOB, GPIO12);
-    // spi_set_nss_low(SPI2);
 
     for (unsigned int i = 0; i < count; i++) {
          uint8_t rx = (uint8_t)spi_xfer(SPI2, buffer[i]);
          buffer[i] = rx;
     }
 
-    /* Wait until not busy */
-    // while (SPI_SR(SPI2) & SPI_SR_BSY);
-
-    // Disable SPI2 to release NSS
-    // spi_disable(SPI2);
-
     gpio_set(GPIOB, GPIO12);
-    // spi_set_nss_high(SPI2);
-
     return *buffer;
 }
 
