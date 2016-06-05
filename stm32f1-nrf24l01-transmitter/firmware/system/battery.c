4231f5d800f595375b37faff59c0f9ba76a661f8
LANE Boys RC <laneboysrc@gmail.com>
2016-06-05 12:15:44 +0800

    Add chart for full discharge and change battery alarms according to findings

--- stm32f1-nrf24l01-transmitter/firmware/system/battery.c
+++ stm32f1-nrf24l01-transmitter/firmware/system/battery.c
@@ -8,8 +8,13 @@
 typedef enum {
     BATTERY_OK,
     BATTERY_LOW,
-    BATTERY_VERY_LOW
+    BATTERY_VERY_LOW,
+    BATTERY_DANGEROUSLY_LOW
 } battery_state_t;
+
+#define BATTERY_LOW_LEVEL 3500
+#define BATTERY_VERY_LOW_LEVEL 3430
+#define BATTERY_DANGEROUSLY_LOW_LEVEL 3340
 
 static battery_state_t battery_state = BATTERY_OK;
 
@@ -17,16 +22,13 @@ static battery_state_t battery_state = BATTERY_OK;
 // ****************************************************************************
 static void battery_alarm_callback(void)
 {
-    // Repeat the alarm in 1 minute
-    SYSTICK_set_callback(battery_alarm_callback, 60 * 1000);
+    // Repeat the alarm in 1 minute, unless when dangerously low, then we
+    // repeat it every 10 seconds
+    SYSTICK_set_callback(battery_alarm_callback,
+        (battery_state != BATTERY_DANGEROUSLY_LOW) ? 60 * 1000 : 10 * 1000);
 
-    if (battery_state == BATTERY_LOW) {
-        MUSIC_play(&song_alarm_battery_low);
-
-    }
-    else {
-        MUSIC_play(&song_alarm_battery_very_low);
-    }
+    MUSIC_play((battery_state == BATTERY_LOW) ?
+        &song_alarm_battery_low : &song_alarm_battery_very_low);
 }
 
 
@@ -39,7 +41,7 @@ void BATTERY_check_level(void)
 
     switch (battery_state) {
         case BATTERY_OK:
-            if (battery_voltage < 3500) {
+            if (battery_voltage < BATTERY_LOW_LEVEL) {
                 battery_state = BATTERY_LOW;
                 LED_flashing();
                 SYSTICK_set_callback(battery_alarm_callback, 1);
@@ -47,12 +49,18 @@ void BATTERY_check_level(void)
             break;
 
         case BATTERY_LOW:
-            if (battery_voltage < 3450) {
+            if (battery_voltage < BATTERY_VERY_LOW_LEVEL) {
                 battery_state = BATTERY_VERY_LOW;
             }
             break;
 
         case BATTERY_VERY_LOW:
+            if (battery_voltage < BATTERY_DANGEROUSLY_LOW_LEVEL) {
+                battery_state = BATTERY_DANGEROUSLY_LOW;
+            }
+            break;
+
+        case BATTERY_DANGEROUSLY_LOW:
         default:
             break;
     }
